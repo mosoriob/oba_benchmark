d4d5a83d8163b3ccbb2994db7ea8d37edb90da16
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-15 13:42:20 -0700

    fix: rename endpoint

--- test_api.sh
+++ test_api.sh
@@ -4,4 +4,4 @@ name="api_cached_enabled_${test_name}"
 file_name="${report_dir}/${name}"
 echo "GET https://api.models.mint.isi.edu/v1.5.0/parameters?username=mint%40isi.edu&page=1&per_page=100" | \
     ./vegeta attack  -duration=${duration} -max-workers ${max_workers}  -rate ${rate} -name=${name} \ |
-    ./vegeta encode > ${file_name}
+    ./vegeta encode > ${file_name}.json
--- test_endpoint.sh
+++ test_endpoint.sh
@@ -1,7 +1,7 @@
 source ./env
 test_name="${duration}_${max_workers}_${rate}"
-name="api_cached_disabled_${test_name}"
+name="endpoint_${test_name}"
 file_name="${report_dir}/${name}"
 cat post | \
     ./vegeta attack  -duration=${duration} -max-workers ${max_workers}  -rate ${rate} -name=${name} \
-    ./vegeta encode > ${file_name}
+    ./vegeta encode > ${file_name}.json
