30cd017eaba43cd926c89df3f1d1057eb53b5aff
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-15 14:37:56 -0700

    typo

--- report.sh
+++ report.sh
@@ -4,5 +4,5 @@ for i in $(find ${report_dir} -name *json); do
     cat $i | ./vegeta report $i > "${filename}.txt"
     cat $i | ./vegeta report -type=hdrplot $i > "${filename}_hdrplot.txt"
     cat $i | ./vegeta plot > ${filename}.html
-    cat $i | vegeta report -type="hist[0,100ms,200ms,300ms,400ms,500ms,600ms,700ms,800ms,900ms,1000ms]"
+    cat $i | ./vegeta report -type="hist[0,100ms,200ms,300ms,400ms,500ms,600ms,700ms,800ms,900ms,1000ms]"
 done
