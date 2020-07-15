0a89dbbfb247d5fbf9111f1d0605026f9ab5c530
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-15 12:14:04 -0700

    avoid tree

--- test_endpoint.sh
+++ test_endpoint.sh
@@ -3,5 +3,5 @@ test_name="${duration}_${max_workers}_${rate}"
 name="api_cached_disabled_${test_name}"
 file_name="${name}"
 cat post | \
-    ./vegeta attack  -duration=${duration} -max-workers ${max_workers}  -rate ${rate} -name=${name} |\
-    tee ${file_name}.bin
+    ./vegeta attack  -duration=${duration} -max-workers ${max_workers}  -rate ${rate} -name=${name} \
+    >> results/${file_name}.bin 
