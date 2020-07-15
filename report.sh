b4d2372defe7d325741447422c429a806cff6e27
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-15 12:23:07 -0700

    add: report

---
+++ report.sh
@@ -0,0 +1,8 @@
+pushd results
+for i in $(ls *.bin); do 
+    cat $i ./vegeta report > $i.txt
+    #vegeta report -type=json $i > $i.json
+    cat $i | ./vegeta -type=hdrplot > ${i}_hdrplot.txt
+    #cat $i | vegeta report -type="hist[0,100ms,200ms,300ms]"
+done
+popd
--- run.sh
+++ run.sh
@@ -4,11 +4,4 @@ sleep 10
 bash test_api_local.sh
 sleep 10
 bash test_endpoint.sh
-pushd results
-for i in $(ls \*.bin); do 
-    cat $i ./vegeta report > $i.txt
-    #vegeta report -type=json $i > $i.json
-    cat $i | ./vegeta -type=hdrplot > ${i}_hdrplot.txt
-    #cat $i | vegeta report -type="hist[0,100ms,200ms,300ms]"
-done
-popd
+bash report.sh
