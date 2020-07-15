993cf605f25ca6e748c7832bce8682e81e174984
Maximiliano Osorio <maxiosorio@gmail.com>
2020-07-15 13:31:57 -0700

    fix: plot html

--- report.sh
+++ report.sh
@@ -3,4 +3,5 @@ for i in $(find ${report_dir} -name *json); do
     filename=${i%.*}
     cat $i | ./vegeta report $i > "${filename}.txt"
     cat $i | ./vegeta report -type=hdrplot $i > "${filename}_hdrplot.txt"
+    cat $i | ./vegeta plot > ${filename}.html
 done
